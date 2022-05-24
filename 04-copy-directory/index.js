e25d2f6f481d89baef16c29783d5f8f35845700d
svereschako <svereschako@gmail.com>
2022-05-24 18:48:40 +0300

    fix: changes in copy-directory func

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,15 @@ const path = require('path');
 const fpath = path.join(__dirname, 'files');
 
 fs.promises.mkdir(path.join(__dirname,"files-copy"), {recursive: true});
+
+fs.readdir(path.join(__dirname,"files-copy"), (err,files) => {
+	files.forEach(file => {
+		fs.unlink(path.join(__dirname,"files-copy", file), err => {
+		    if (err) throw err;
+		});
+	});
+});
+
 fs.readdir(fpath, (err, files) => {
 	files.forEach(file => {
 		fs.promises.copyFile(path.join(__dirname,"files",file), path.join(__dirname,"files-copy",file));
