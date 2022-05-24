f57c2e77efe6c0ec07ccc0e98e5f945782371654
svereschako <svereschako@gmail.com>
2022-05-24 18:26:08 +0300

    fix: changes in read-file function

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,4 +5,4 @@ const filepath = path.join(__dirname, 'text.txt');
 const stream = fs.createReadStream(filepath, 'utf-8');
 let data = '';
 stream.on('data', chunk => data += chunk);
-stream.on('end', () => console.log('End', data));
+stream.on('end', () => console.log(data));
