745669d04b6468931c2b99f1e8ff6eb3a0df5890
svereschako <svereschako@gmail.com>
2022-05-24 15:36:34 +0300

    fix: changes in copy function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,13 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const re = /\{\{\w+\}\}/gi;
-const FSP = require('fs').promises;
-//const fse = require('fs-extra');
-//const re = /header/gi;
 let components = {};
 let data = "";
 let styles = "";
-//let mx = "lkjfiowejf{{eifwoefj}}owefjowef{{header}}wioejfoiwejf{{footer}}fjowiejfiowejfoiwefjiowefj";
 
 fs.promises.mkdir(path.join(__dirname,"project-dist"), {recursive: true});
 const stream = fs.createReadStream(path.join(__dirname,"template.html"),"utf-8");
@@ -18,9 +14,7 @@ function replace() {
 	var ndata = data.replace(re, function(match){
 		match = match.substring(2, match.length-2);
 		return components[match];
-	});
-	//var ndata = data.replace(re, "hello");
-	//console.log( typeof data);
+	});	
 	fs.promises.writeFile(path.join(__dirname, "project-dist", "index.html"), ndata);	
 }
 
@@ -31,9 +25,6 @@ function getContent(name){
 	strm.on('data', chunk => content += chunk);
 	return strm.on('end', () => content);
 }
-
-
-
 
 function load() {
 	fs.readdir(path.join(__dirname, "components"), {withFileTypes: true}, (err, files) => {
@@ -62,13 +53,7 @@ fs.readdir(path.join(__dirname, "styles"), {withFileTypes: true}, (err, files) =
 	});
 });
 
-/*fs.readdir(path.join(__dirname, "assets"), (err, files) => {
-	files.forEach(file => {
-		fs.promises.copyFile(path.join(__dirname,"assets",file), path.join(__dirname,"project-dist", "assets",file));
-	});
-});*/
-
-function copy(from, to) {
+/*function copy(from, to) {
 	fs.readdir(from, {withFileTypes: true}, (err, files) => {		
 		files.forEach(file => {			
 			if(file.isDirectory()){
@@ -83,20 +68,20 @@ function copy(from, to) {
 			}						
 		});
 	});
-}
+}*/
 
 //copy(path.join(__dirname,"assets"), path.join(__dirname,"project-dist", "assets"));
 
 async function copyDir(src,dest) {
-    const entries = await FSP.readdir(src, {withFileTypes: true});
-    await FSP.mkdir(dest, {recursive: true});
+    const entries = await fs.promises.readdir(src, {withFileTypes: true});
+    await fs.promises.mkdir(dest, {recursive: true});
     for(let entry of entries) {
         const srcPath = path.join(src, entry.name);
         const destPath = path.join(dest, entry.name);
         if(entry.isDirectory()) {
             await copyDir(srcPath, destPath);
         } else {
-            await FSP.copyFile(srcPath, destPath);
+            await fs.promises.copyFile(srcPath, destPath);
         }
     }
 }
