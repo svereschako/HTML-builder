3103809f0fa6ba12a6425d412327b19cdd3c7793
svereschako <33933400+svereschako@users.noreply.github.com>
2023-05-09 03:56:39 +0200

    refactor: clean code

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,12 +4,10 @@ const { stdin, stdout } = process;
 const filepath = path.join(__dirname, 'dest.txt');
 const output = fs.createWriteStream(filepath);
 
-
 stdout.write('Enter your message\n');
 stdin.on('data', data => {
 	if(data.toString().trim().toLowerCase() === "exit")
-		process.exit();
-	//console.log(data.toString());       
+		process.exit();	       
 	output.write(data + "\n");	
 });
 process.on('exit', () => stdout.write('Удачи!'));
