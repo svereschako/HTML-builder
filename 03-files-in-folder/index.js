6a883a8437d320e0d6152b15f2c621e8cb970530
svereschako <33933400+svereschako@users.noreply.github.com>
2024-01-23 12:36:22 +0200

    feat: add error handlers

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,9 +3,11 @@ const path = require('path');
 const fpath = path.join(__dirname, 'secret-folder');
 
 fs.readdir(fpath, {withFileTypes: true}, (err, files) => {
+    if (err) throw err;
     files.forEach(file => {		
       if(file.isFile()){      	
-      	fs.stat(path.join(__dirname,"secret-folder",file.name), function(err, stats) {      		
+      	fs.stat(path.join(__dirname,"secret-folder",file.name), function(err, stats) {
+            if (err) throw err;
       	    console.log(path.parse(file.name)["name"] + " - " + path.parse(file.name)["ext"].slice(1) + " - " + stats["size"]);      	    
       	});
       }      
